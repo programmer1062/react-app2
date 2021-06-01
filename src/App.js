d4f10c9143d3729d23d1e39307dc7a45435cae4b
Shubham Raj <SINGH29199810@GMAIL.COM>
2021-06-01 11:18:30 +0530

    Modal Show Example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Carousel } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 export default function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const displayModal = () => setModalShow(!modalShow);
   return (
     <div>
-      <h2 className="bg-dark text-light p-2">React Booststrap CarouselEx</h2>
+      <h1 className="bg-dark text-light p-2">Modal demo</h1>
 
-      <Carousel>
-        {[1, 2, 3].map((item, index) => (
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={"https://picsum.photos/200" + index}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      {[1, 2, 2, 3, 4].map((item, index) => (
+        <div className="mb-1" key={index}>
+          <Button variant="secondary" onClick={() => displayModal()}>
+            Employee Details {index + 1}
+          </Button>
+        </div>
+      ))}
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      ></MyVerticallyCenteredModal>
     </div>
+  );
+}
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
