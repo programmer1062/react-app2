38d32cbd07b9b9e10b5d67baff71d0512b0961d9
Shubham Raj <SINGH29199810@GMAIL.COM>
2021-06-01 10:10:41 +0530

    v1 commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Alert, Button } from "react-bootstrap";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-align p-2">React Bootstrap</h1>
+      <Button variant="outline-success" size="lg">
+        React Button
+      </Button>
+      <Alert variant="primary">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+        quos adipisci error illo, possimus, culpa blanditiis consequatur commodi
+        ipsum quis minus ex sunt quaerat, atque earum quisquam voluptatum eum
+        explicabo!
+      </Alert>
+      <Alert variant="primary">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+        quos adipisci error illo, possimus, culpa blanditiis consequatur commodi
+        ipsum quis minus ex sunt quaerat, atque earum quisquam voluptatum eum
+        explicabo!
+      </Alert>
     </div>
   );
 }
-
-export default App;
